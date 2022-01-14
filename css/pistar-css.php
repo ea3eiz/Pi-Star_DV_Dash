05131e67386e8fb5355588d9002aa04b6e799c69
Manel <ea3eiz@ea3eiz.com>
2022-01-14 11:16:10 +0100

    1

--- css/pistar-css.php
+++ css/pistar-css.php
@@ -315,9 +315,9 @@ h1 {
 input.toggle-round-flat + label {
     padding: 1px;
     border: 1px solid transparent;
-    width: 36px;                    /* Anchura del check */
-    height: 18px;                   /* Altura del check */
-    background-color: #FFF;      /* Color border check Apagado */
+    width: 33px;
+    height: 18px;
+    background-color: #dddddd;
     border-radius: 10px;
     transition: background 0.4s;
 }
@@ -334,9 +334,9 @@ input.toggle-round-flat + label:before {
     left: 1px;
     bottom: 1px;
     right: 1px;
-    background-color: #000;     /* Fondo check apagado o encendido */
-
-
+    background-color: #fff;
+    background: <?php echo $backgroundContent; ?>;
+    border-radius: 10px;
     transition: background 0.4s;
 }
 
@@ -345,18 +345,18 @@ input.toggle-round-flat + label:after {
     left: 2px;
     bottom: 2px;
     width: 16px;
-    background-color: #f00;     /* Color rojo cuadradito check apagado */ 
+    background-color: #dddddd;
     border-radius: 12px;
     transition: margin 0.4s, background 0.4s;
 }
 
 input.toggle-round-flat:checked + label {
-    background-color: #339933;     /* Color del border del check encendido cusdradito verde */
+    background-color: <?php echo $backgroundBanners; ?>;
 }
 
 input.toggle-round-flat:checked + label:after {
-    margin-left: 18px;   /* Desplazamiento de los cuadraditos verde o rojo a la derecha */
-    background-color: #339933; /* Color del cuadradito verde check encendido;
+    margin-left: 14px;
+    background-color: <?php echo $backgroundBanners; ?>;
 }
 
 input.toggle-round-flat:focus + label {
